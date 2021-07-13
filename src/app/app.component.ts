f15dbae76b7e3f391b28a831f1e593beee39536c
Arirubiastor <arianarubigo@gmail.com>
2021-07-13 18:19:16 +0200

    create new components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+// import { builtinModules } from 'module';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
+  // styles: [`
+  //   h3 {
+  //     color: darkblue;
+  //   }
+  // `]
 })
 export class AppComponent {
-  title = 'my-first-angular-app';
+  name = 'Ari';
 }
